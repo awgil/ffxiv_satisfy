4bd817532341dd9169795b39edf1b547eba7771c
Jackson <9527380+Jaksuhn@users.noreply.github.com>
2025-11-05 11:48:38 +0000

    Update GatherData.cs

--- vsatisfy/GatherData.cs
+++ vsatisfy/GatherData.cs
@@ -1,5 +1,6 @@
 ﻿using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using Lumina.Excel.Sheets;
+using System.Numerics;
 
 namespace Satisfy;
 
@@ -7,10 +8,31 @@ public sealed class GatherData
 {
     public uint GatherItemId;
     public uint ClassJobId => GetGatherer();
+    public GatherPoint[] GatherPoints = [];
 
     public GatherData(uint itemId)
     {
         GatherItemId = itemId;
+        if (Service.LuminaRow<GatheringItem>(GatherItemId) is { RowId: var item } && Service.LuminaSubrows<GatheringItemPoint>(item) is { } points)
+            foreach (var point in points)
+                GatherPoints = [.. GatherPoints, GatherPoint.FromSubrow(point)];
+    }
+
+    public record struct GatherPoint(uint TerritoryId, Vector2 Position, uint Radius, uint ClassJob)
+    {
+        public static GatherPoint FromSubrow(GatheringItemPoint point)
+        {
+            var exportedPoint = Service.LuminaRow<ExportedGatheringPoint>(point.GatheringPoint.Value.GatheringPointBase.RowId)!;
+            var pos = new Vector2(exportedPoint.Value.X, exportedPoint.Value.Y);
+            var classJob = exportedPoint.Value.GatheringType.RowId switch
+            {
+                0 or 1 => 16u,
+                2 or 3 => 17u,
+                4 or 5 => 18u,
+                _ => throw new Exception($"Unknown gathering type {exportedPoint.Value.GatheringType.RowId}"),
+            };
+            return new(point.GatheringPoint.Value.TerritoryType.RowId, pos, exportedPoint.Value.Radius, classJob);
+        }
     }
 
     private unsafe uint GetGatherer()
